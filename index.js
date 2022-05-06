243e0452abb0f15ae57f40420531fc27fc57f905
Manuel Gallego <man.gallegojz@outlook.com>
2022-05-05 22:13:57 -0300

    prueba2

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 //paquetes externos
 
-<<<<<<< HEAD
 const express = require("express")
 const multer = require("multer")
 
@@ -12,73 +11,6 @@ const app = express()
 
 app.set('views', './view');
 app.set('view engine', 'pug');
-=======
-const multer = require("multer");
-const handlebars = require("express-handlebars");
-const express = require("express");
-const app = express();
-
-//server
-
-const http = require("http");
-const server = http.createServer(app);
-
-//socket
-
-const {Server} = require("socket.io");
-const io = new Server(server);
-
-//Storage multer
-
-const storage = multer.diskStorage({
-  destination:(req, file, cb)=>{
-    cb(null, "uploads")
-  },
-  filename:(req, file, cb)=>{
-    cb(null, file)
-  }
-})
-
-let upload = multer({storage});
-
-//número de puerto
-
-const PORT = process.env.PORT || 8080;
-
-//rutas
-
-const tiendaRutas = require("./routes/tienda")
-const carritoRutas = require("./routes/carrito")
-const informacionRutas = require("./routes/informacion");
-
-//tienda
-const Contenedor = require("./classes/contenedor.class");  
-const nuevoArchivo = new Contenedor("./productos.json");
-
-//conexion
-
-io.on("connection", (socket)=>{
-  socket.emit("message_backend", nuevoArchivo.getAll())
-
-  socket.on("message_cliente", (data)=>{
-    console.log(data)
-  })
-})
-
-//seteo la plantilla
-
-app.engine(
-  "hbs",
-  handlebars.engine({
-    extname: "hbs",
-    layoutsDir: __dirname + "/views/layouts",
-    defaultLayout: "index",
-    partialsDir: __dirname + "/views/partials",
-  })
-)
-app.set('views', './views');
-app.set('view engine', 'hbs');
->>>>>>> checkout
 
 //JSON
 
@@ -105,7 +37,6 @@ try {
   console.log("Se produjo el error:" + error);
 }
 
-<<<<<<< HEAD
 
 app.get("/api/productosinicio", (req, res) => {
     res.render("index");
@@ -132,8 +63,3 @@ app.post("/api/productosinicio/formulario", (req, res)=>{
   nuevoArchivo.save(req.body)
   res.render("formPage", {guardado: true, data: nuevoArchivo.getAll(), eliminar: nuevoArchivo.deleteById()})
 })
-=======
-app.get("/api/inicio", (req, res) => {
-    res.render("inicio");
-  })
->>>>>>> checkout
